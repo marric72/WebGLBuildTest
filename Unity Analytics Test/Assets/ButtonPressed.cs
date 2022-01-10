ae55cbea6013e8e962128bc3164e4ae63275f100
Heather Marriott <heathermarriott1@gmail.com>
2022-01-09 19:58:48 -0700

    added Unity Analytics

--- Unity Analytics Test/Assets/ButtonPressed.cs
+++ Unity Analytics Test/Assets/ButtonPressed.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Analytics;
 
 public class ButtonPressed : MonoBehaviour
 {
@@ -22,9 +23,21 @@ public class ButtonPressed : MonoBehaviour
     public void Fcn1()
     {
         myLabel.GetComponent<Text>().text = "Button1 Pressed";
+        //an example of sending a string to Analytics
+        AnalyticsResult result=Analytics.CustomEvent("Button1 pressed");
+        Debug.Log("analyticsResult:" + result);
     }
     public void Fcn2()
     {
         myLabel.GetComponent<Text>().text = "Button2 Pressed";
+        //an example of sending a dictionary to Analytics
+        AnalyticsResult result = Analytics.CustomEvent(
+            "ButtonPressInfo",
+            new Dictionary<string, object>{
+                { "Button", "2" },
+                { "UserID", "TBD" } });
+
+                ;
+        Debug.Log("analyticsResult:" + result);
     }
 }
